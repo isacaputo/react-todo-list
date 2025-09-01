69f3894ca7b2a1976fde67f0f721b6df1f396b81
isacaputo <isadora.caputo@gmail.com>
2025-09-01 19:15:48 +0200

    fix: standardize color quotes in theme configuration

--- src/providers/Theme/theme.config.js
+++ src/providers/Theme/theme.config.js
@@ -1,22 +1,22 @@
 export const theme = {
   colors: {
     primary: {
-      main: "#F66605",
-      light: "#FFF3EC",
+      main: '#0b656aff',
+      light: '#d2e9eaff',
     },
     secondary: {
-      main: "#069094",
-      light: "#ECF9FA",
+      main: '#069094',
+      light: '#ECF9FA',
     },
     misc: {
-      w0: "#E6E6E6",
-      w1: "#E7E7Ed",
-      w2: "#272833",
-      w3: "#D8E3E4",
-      w4: "#EBECEF",
-      w5: "#F7F8F9",
-      w6: "#A7A9BC",
-      w7: "#495057",
+      w0: '#E6E6E6',
+      w1: '#E7E7Ed',
+      w2: '#272833',
+      w3: '#D8E3E4',
+      w4: '#EBECEF',
+      w5: '#F7F8F9',
+      w6: '#A7A9BC',
+      w7: '#495057',
     },
   },
 };
