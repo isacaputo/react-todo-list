71aa6167ca4ba7fc8bac17215122e74429777ef1
isacaputo <isadora.caputo@gmail.com>
2025-09-01 19:16:11 +0200

    fix: simplify error message in useTodo hook

--- src/providers/Todo/useTodo.js
+++ src/providers/Todo/useTodo.js
@@ -4,9 +4,7 @@ import { TodoContext } from "./TodoContext";
 export const useTodo = () => {
   const context = React.useContext(TodoContext);
   if (!context) {
-    throw Error(
-      "The `useTodo` hook needs of <TodoProvider /> to get the context"
-    );
+    throw Error("The `useTodo` hook needs <TodoProvider /> to get the context");
   }
   return context;
 };
